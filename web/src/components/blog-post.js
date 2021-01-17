03afe1277ee8c32c97ff20995583a8924cfc09b6
Carolyn <carolyn@almende.org>
2021-01-17 21:10:39 +0100

    remove categories and authors

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,10 +1,8 @@
-import { format, distanceInWords, differenceInDays } from "date-fns";
 import React from "react";
 import { buildImageObj } from "../lib/helpers";
 import { imageUrlFor } from "../lib/image-url";
 import PortableText from "./portableText";
 import Container from "./container";
-import AuthorList from "./author-list";
 import RecipeMetric from "./recipe-metric";
 import { GiCookingPot, GiForkKnifeSpoon } from "react-icons/gi";
 import { RiKnifeFill } from "react-icons/ri";
@@ -12,7 +10,7 @@ import { RiKnifeFill } from "react-icons/ri";
 import styles from "./blog-post.module.css";
 
 function BlogPost(props) {
-  const { _rawBody, _rawExcerpt, authors, categories, title, mainImage, publishedAt } = props;
+  const { _rawBody, _rawExcerpt, title, mainImage } = props;
   return (
     <article className={styles.root}>
       {mainImage && mainImage.asset && (
@@ -40,31 +38,11 @@ function BlogPost(props) {
         </div>
       )}
       <Container>
-        <div className={styles.grid}>
+        <div>
           <div className={styles.mainContent}>
             <h1 className={styles.title}>{title}</h1>
             {_rawBody && <PortableText blocks={_rawBody} />}
           </div>
-          <aside className={styles.metaContent}>
-            {publishedAt && (
-              <div className={styles.publishedAt}>
-                {differenceInDays(new Date(publishedAt), new Date()) > 3
-                  ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), "MMMM Do, YYYY")}
-              </div>
-            )}
-            {authors && <AuthorList items={authors} title="Authors" />}
-            {categories && (
-              <div className={styles.categories}>
-                <h3 className={styles.categoriesHeadline}>Categories</h3>
-                <ul>
-                  {categories.map((category) => (
-                    <li key={category._id}>{category.title}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </aside>
         </div>
       </Container>
     </article>
