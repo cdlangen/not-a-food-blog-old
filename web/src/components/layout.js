18bf1d4beb24f3821c5833017d36fe987bfaf018
Carolyn <carolyn@almende.org>
2021-01-22 23:02:35 +0100

    Use google fonts via Helmet

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import Header from './header'
+import React from "react";
+import Helmet from "react-helmet";
+import Header from "./header";
 
-import '../styles/layout.css'
-import styles from './layout.module.css'
+import "../styles/layout.css";
+import styles from "./layout.module.css";
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
   <>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.siteInfo}>
-          &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
+          &copy; {new Date().getFullYear()}, Built with <a href="https://www.sanity.io">Sanity</a>{" "}
           &amp;
           {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </div>
       </div>
     </footer>
   </>
-)
+);
 
-export default Layout
+export default Layout;
