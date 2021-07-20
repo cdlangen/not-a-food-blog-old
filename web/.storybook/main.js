ccaec0c1cd8a4cc2c9e72116d7dbcf2d804cfaa9
Carolyn <carolyn@almende.org>
2021-07-20 09:53:33 +0200

    Move storybook to web directory

---
+++ web/.storybook/main.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    // "../src/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  webpackFinal: async config => {
+    // Remove the existing css rule
+    config.module.rules = config.module.rules.filter(
+      f => f.test.toString() !== '/\\.css$/'
+    );
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', {
+        loader: 'css-loader',
+        options: {
+          modules: true, // Enable modules to help you using className
+        }
+      }],
+      include: path.resolve(__dirname, '../src/components'),
+    });
+
+    return config;
+  },
+}
--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/components/recipe.module.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
