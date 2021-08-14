b6c0c81af0334bc0c8c4a8b39ab42a732c3986e3
Carolyn <carolyn@almende.org>
2021-08-14 19:35:41 +0200

    fix recipe-header styling

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,31 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  "stories": [
+  stories: [
     // "../src/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  webpackFinal: async config => {
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss"],
+  webpackFinal: async (config) => {
     // Remove the existing css rule
-    config.module.rules = config.module.rules.filter(
-      f => f.test.toString() !== '/\\.css$/'
-    );
+    config.module.rules = config.module.rules.filter((f) => f.test.toString() !== "/\\.css$/");
 
     config.module.rules.push({
       test: /\.css$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          modules: true, // Enable modules to help you using className
-        }
-      }],
-      include: path.resolve(__dirname, '../src/components'),
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true, // Enable modules to help you using className
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../src/components"),
     });
 
     return config;
   },
-}
+};
